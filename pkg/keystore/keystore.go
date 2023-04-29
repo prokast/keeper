1f1962d4cf96e140a129743683bab852552de8b0
traksel <goshatraksel@yandex.ru>
2023-04-29 21:46:14 +0300

    Update keystore

--- pkg/keystore/keystore.go
+++ pkg/keystore/keystore.go
@@ -3,11 +3,28 @@ package keystore
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
+type certInfo struct {
+	subject  struct {
+        notAfter string
+    }
+}
+
+func getFiles() []string {
+	var filenames []string
+	files, _ := ioutil.ReadDir("/tmp/")
+	for _, file := range files {
+		filenames = append(filenames, file.Name())
+	}
+	return filenames
+}
+
 func GetCertInfo() {
+    for
 	bs, _ := os.ReadFile("cert.pem") // handle error
 
 	block, _ := pem.Decode(bs)
