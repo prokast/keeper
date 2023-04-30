906a2c42856362b899fb83aa331b9ca76ca14d2c
traksel <goshatraksel@yandex.ru>
2023-04-30 15:29:02 +0300

    Parse certs dir

--- pkg/keystore/keystore.go
+++ pkg/keystore/keystore.go
@@ -3,38 +3,49 @@ package keystore
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
 
-type certInfo struct {
-	subject  struct {
-        notAfter string
-    }
+type CertInfo struct {
+	Subject  interface{}
+	NotAfter time.Time
 }
 
 func getFiles() []string {
 	var filenames []string
-	files, _ := ioutil.ReadDir("/tmp/")
+	files, _ := ioutil.ReadDir("./etc/certs")
 	for _, file := range files {
 		filenames = append(filenames, file.Name())
 	}
+	fmt.Println(filenames)
 	return filenames
 }
 
 func GetCertInfo() {
-    for
-	bs, _ := os.ReadFile("cert.pem") // handle error
-
-	block, _ := pem.Decode(bs)
-	if block == nil {
-		log.Fatal("failed to parse PEM block containing the public key")
+	ci := []CertInfo{}
+	files := getFiles()
+	for _, file := range files {
+		bs, _ := os.ReadFile("/Users/traksel/git/go/src/keeper/etc/certs/" + file)
+		block, _ := pem.Decode(bs)
+		if block == nil {
+			log.Fatal("failed to parse PEM block containing the public key")
+		}
+		cert, _ := x509.ParseCertificate(block.Bytes)
+		ci = append(ci, struct {
+			Subject  interface{}
+			NotAfter time.Time
+		}{
+			Subject:  cert.Subject.CommonName,
+			NotAfter: cert.NotAfter,
+		})
 	}
+	// handle error
 
-	cert, _ := x509.ParseCertificate(block.Bytes) // handle error
-
-	log.Printf("Subject:   %q", cert.NotAfter)
+	log.Printf("Subject:   %q", ci)
 
 	// Subject:   "CN=*.google.com"
 }
